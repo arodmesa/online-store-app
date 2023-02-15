f7a153660a72825bfaf904e4cdfbbb2a23605c98
Alex <arodmesa@gmail.com>
2023-02-15 17:12:35 +0100

    PortraitDetailsPage css completed || minor bugs fixed

--- src/websitePages/PortraitDetailsPage.js
+++ src/websitePages/PortraitDetailsPage.js
@@ -1,4 +1,5 @@
 import Animation from '../componentes/Animation';
+import BackgroundForNavBar from '../features/backgroundForNavBar/BackgroundForNavBar';
 import './PortraitDetailsPage.css';
 import './HomePage.css';
 import { fetchAndChangePortraitDetails } from '../features/portraitDetails/portraitDetailsFunctionalities';
@@ -17,28 +18,35 @@ function PortraitDetailsPage(){
         return(<Animation/>);
     }
     return(
-        <div className="div_details">
-            <img className='img_detalles' src={portraitDetails.webformatURL} alt='imagen product'></img>
-            <div className='org_detalles'>
-                <div className='org_vMovil'>
-                    <h1 className='h1_details'>{portraitDetails.tags}</h1>
-                    <h1 className='h1_details'>{`$${portraitDetails.webformatHeight}`}</h1>
-                    <h4 className='h4_details'>{`${portraitDetails.likes} likes`}</h4>
-                    <h4 className='h4_details'>{`${portraitDetails.comments} comments`}</h4>
-                    <h4 className='h4_details'>{`${portraitDetails.downloads} purchases`}</h4>
-                    <h4 className='h4_details'>Available: <i>In stock</i></h4>
-                    <h4 className='h4_details'>Author: <i>{portraitDetails.user}</i></h4>
-                </div>                
-                <div className='org_things'>
-                    <div className='org_cant'>
-                        <i className="fa fa-minus signo" onClick={()=>dispatch(decrementDefaultAmount())}></i>
-                        <h3 className='h3_details'>{defaultAmountOfPortraitsToAddCart}</h3>
-                        <i className="fa fa-plus signo" onClick={()=>dispatch(incrementDefaultAmount())}></i>
+       <>
+            <BackgroundForNavBar />
+            <div className="divDetailsPage">
+                <div className='divColumn'>
+                    <img className='imageDetails' src={portraitDetails.webformatURL} alt='imagen product'></img>
+                    <div className='organizeTags'>
+                            <h1 className='detailsTags'>{portraitDetails.tags}</h1>
+                            <h1 className='detailsTags'>{`$${portraitDetails.webformatHeight}`}</h1>
                     </div>
-                    <button className='btn_home' type='button' onClick={()=>dispatch(addPortraitToCart({portraitDetails, defaultAmountOfPortraitsToAddCart}))}>Add to cart</button>
-                </div>                
+                </div>
+                <div className='organizeContent'>
+                    <div className='organizeDetails'>                     
+                        <h4 className='details'>{`${portraitDetails.likes} likes`}</h4>
+                        <h4 className='details'>{`${portraitDetails.comments} comments`}</h4>
+                        <h4 className='details'>{`${portraitDetails.downloads} purchases`}</h4>
+                        <h4 className='details'>Available: <i>In stock</i></h4>
+                        <h4 className='details'>Author: <i>{portraitDetails.user}</i></h4>
+                    </div>                
+                    <div className='divColumn'>
+                        <div className='alignAmountComponents'>
+                            <i className="fa fa-minus signs" onClick={()=>dispatch(decrementDefaultAmount())}></i>
+                            <h3 className='amount'>{defaultAmountOfPortraitsToAddCart}</h3>
+                            <i className="fa fa-plus signs" onClick={()=>dispatch(incrementDefaultAmount())}></i>
+                        </div>
+                        <button className='addCart' type='button' onClick={()=>dispatch(addPortraitToCart({portraitDetails, defaultAmountOfPortraitsToAddCart}))}>Add to cart</button>
+                    </div>      
+                </div>
             </div>
-        </div>
+       </>
     );
 }
 export default PortraitDetailsPage;

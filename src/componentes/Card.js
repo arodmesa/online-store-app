3fc4dcb71ec73ca17aa84c75c5794af8e8b487a6
Alex <arodmesa@gmail.com>
2023-02-20 00:25:05 +0100

    Minor css changes for improving mobile view

--- src/componentes/Card.js
+++ src/componentes/Card.js
@@ -3,7 +3,7 @@ function Card({iconCard, titleCard, textCard}){
     return(
         <div className="divCard divColumn">
             <img className='imgCard' src={iconCard} alt='icono'></img>      
-            <div className='divColumn organizeTextInCard'>
+            <div className='divColumnCard organizeTextInCard'>
                 <h3 className='cardTitle'>{titleCard}</h3>
                 <p className='cardText'>{textCard}</p>
             </div>                 
